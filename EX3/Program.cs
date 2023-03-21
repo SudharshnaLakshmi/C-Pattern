2de7d8a5fc91608b7675ba5189c12325cbfd4e46
Sudharshna Lakshmi.S <sudharshnalakshmi@gmail.com>
2023-03-21 16:26:16 +0530

    EX03

--- EX3/Program.cs
+++ EX3/Program.cs
@@ -1,27 +1,4 @@
-# Pattern
-
-## Aim:
-To write a C# program for a pascal's triangle.
-
-## Equipment Required:
-Microsoft Visual Studio 2022
-
-## Algorithm:
-1. Start the program.
-2. Get the limit from the user.
-3. Use for loop to print the rows , columns and space.
-4. Use if-else condition inside the loop to print the values.
-5. Use val = val * (i - j + 1) / j to print the inner value.
-6. Using Console.write print the Pascal's triangle.
-7. End the program.
-
-## Program:
-```
-DEVELOPED BY : SUDHARSHNA LAKSHMI S
-REGISTER NO : 212221230110
-```
-```
-using System;
+﻿using System;
 public class EX3
 {
     public static void Main()
@@ -46,10 +23,3 @@ public class EX3
         }
     }
 }
-```
-
-## Output:
-![OUT](1.png)
-
-## Result:
-A C# program for a pascal's triangle is executed successfully.
